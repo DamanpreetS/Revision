c80749fa60aac572b1993c585329628c26a06f28
DamanpreetS <preethanjhra63@gmail.com>
2022-12-29 12:32:49 +0530

    yes

--- src/test/java/cucumberTest/TestRunner.java
+++ src/test/java/cucumberTest/TestRunner.java
@@ -5,6 +5,9 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 
 @CucumberOptions(features="src/test/java/cucumberTest", glue="stepDefinitions")
 public class TestRunner extends AbstractTestNGCucumberTests{
-	
+	private void syso() {
+		// TODO Auto-generated method stub
+
+	}
 
 }
